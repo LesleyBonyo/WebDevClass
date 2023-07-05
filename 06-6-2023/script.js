c042e35348cff1e1f66cfc6893f4c3363005f13e
LesleyBonyo <lesleybonyo@gmail.com>
2023-07-05 19:40:30 +0300

    Form Validation

--- 06-6-2023/script.js
+++ 06-6-2023/script.js
@@ -11,18 +11,29 @@ function validateForm(){
 		document.getElementById('userpassword').value;
 	if (firstName == "" || lastName == "" || email == "" || password == "") {
 		alert("Field should not be blank");
-	} else if (!firstName.match(/^[A-Za-z]+$/)) {
+		return false;
+	} 
+	if (!firstName.match(/^[A-Za-z]+$/)) {
 		document.getElementById('result1').innerHTML = 
 			"First Name should have alphabetic characters only";
 		return false;
-	} else if (password.length < 8){
+	} else{
+		document.getElementById('result1').innerHTML = "";
+		
+	}
+	if (password.length < 8){
 		document.getElementById('result2').innerHTML = 
 			"Password should have at least 8 characters only";
 		return false;
-	} else {
-		alert("success");
+	} else{
+		document.getElementById('result2').innerHTML = "";
+		
 	}
 
+/*show what is displayed when the user 
+				puts correct password length */
+	
+	alert("success");
 	return true;
 
 }
